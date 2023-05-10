4b8da2f805654aceabd366708247e711952a5703
Rishika0101 <124979948+Rishika0101@users.noreply.github.com>
2023-05-10 11:46:16 +0530

    Update Example.kt

--- nullable/Example.kt
+++ nullable/Example.kt
@@ -7,14 +7,14 @@ fun main(){
     // what if we directly assign a null value ...we will
     // face an error which shows that can't assign null to non-nullable type.
 
-      //val text1:String = null
+    //val text1:String = null
     val text1:String? = null
     println("Text1 value when it holds null value is: $text1")
 
     val text2:String = null.toString()
     println("Without defining it as null: $text2")
     
-     // Using non-null asserted operator or !! .
+    // Using non-null asserted operator or !! .
     var nullableString: String? = "Hello, Coders!"
     println("Length of the nullable string is: $nullableString")
 
